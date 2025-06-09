9cd17ab0117cab012815826274b4fd6a91dc8e43
MMCyber01 <matheus.74726473@gmail.com>
2025-06-09 18:05:47 -0300

    Update hardhat.config.js

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,1 +1,19 @@
-// Configuração Hardhat
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    bscTestnet: {
+      url: process.env.BSC_TESTNET_RPC,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    ethereum: {
+      url: process.env.ETH_RPC,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  }
+};
