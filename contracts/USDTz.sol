0b1113c8ac9fd0f18482b830f910ea91775288ce
MMCyber01 <matheus.74726473@gmail.com>
2025-06-09 18:21:18 -0300

    USDTz.sol

--- contracts/USDTz.sol
+++ contracts/USDTz.sol
@@ -1,1 +1,88 @@
-// ERC20 customizado
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+interface AggregatorV3Interface {
+    function latestRoundData()
+        external
+        view
+        returns (
+            uint80 roundID,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        );
+}
+
+contract USDTz {
+    string public name = "USDT.z";
+    string public symbol = "USDTz";
+    uint8 public decimals = 18;
+    uint256 public totalSupply;
+    address public owner;
+
+    AggregatorV3Interface public priceFeed;
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not the owner");
+        _;
+    }
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    constructor(uint256 initialSupply) {
+        owner = msg.sender;
+        totalSupply = initialSupply;
+        balanceOf[msg.sender] = initialSupply;
+
+        priceFeed = AggregatorV3Interface(
+            0x8Ac9327433e3AC1069470fFFCA8dbB6cf2604632 // BSC Testnet USDT/USD
+        );
+    }
+
+    function getLatestPrice() public view returns (int256) {
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+        return price;
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        totalSupply += amount;
+        balanceOf[to] += amount;
+        emit Transfer(address(0), to, amount);
+    }
+
+    function burn(uint256 amount) external onlyOwner {
+        require(balanceOf[msg.sender] >= amount, "Not enough balance");
+        balanceOf[msg.sender] -= amount;
+        totalSupply -= amount;
+        emit Transfer(msg.sender, address(0), amount);
+    }
+
+    function transfer(address to, uint256 value) public returns (bool) {
+        require(balanceOf[msg.sender] >= value, "Not enough balance");
+        balanceOf[msg.sender] -= value;
+        balanceOf[to] += value;
+        emit Transfer(msg.sender, to, value);
+        return true;
+    }
+
+    function approve(address spender, uint256 value) public returns (bool) {
+        allowance[msg.sender][spender] = value;
+        emit Approval(msg.sender, spender, value);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 value) public returns (bool) {
+        require(value <= balanceOf[from], "Insufficient balance");
+        require(value <= allowance[from][msg.sender], "Allowance exceeded");
+        balanceOf[from] -= value;
+        balanceOf[to] += value;
+        allowance[from][msg.sender] -= value;
+        emit Transfer(from, to, value);
+        return true;
+    }
+}
